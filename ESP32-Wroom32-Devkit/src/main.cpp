c5c69efaa128a1a1328e26ac1d962377604f8749
MSI\ADMINS <thanh.nguyenthinh@hcmut.edu.vn>
2025-11-08 16:56:43 +0700

    Blinky led test

--- ESP32-Wroom32-Devkit/src/main.cpp
+++ ESP32-Wroom32-Devkit/src/main.cpp
@@ -1,18 +1,19 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED 13
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(LED, HIGH);
+  Serial.println("LED is on");
+  delay(1000);
+  digitalWrite(LED, LOW);
+  Serial.println("LED is off");
+  delay(1000);
 }
