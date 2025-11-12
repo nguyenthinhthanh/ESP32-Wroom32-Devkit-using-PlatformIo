84d8c8b1c604923193702b4517846239f5d3987a
MSI\ADMINS <thanh.nguyenthinh@hcmut.edu.vn>
2025-11-12 17:56:53 +0700

    Add tiny ml task

--- ESP32-Wroom32-Devkit/src/main.cpp
+++ ESP32-Wroom32-Devkit/src/main.cpp
@@ -4,6 +4,7 @@
 #include "temp_humi_monitor.h"
 
 #include "main_server.h"
+#include "tiny_ml.h"
 
 void setup()
 {
@@ -11,10 +12,9 @@ void setup()
   // check_info_File(0); Need to read here
 
   xTaskCreate(led_blinky, "Task LED Blink", 2048, NULL, 2, NULL);
-  // xTaskCreate(neo_blinky, "Task NEO Blink", 2048, NULL, 2, NULL);
-  xTaskCreate(temp_humi_monitor, "Task TEMP HUMI Monitor", 2048, NULL, 2, NULL);
+  xTaskCreate(temp_humi_monitor, "Task Temperature Huminity Monitor", 2048, NULL, 2, NULL);
   xTaskCreate(main_server_task, "Task Main Server" ,8192  ,NULL  ,2 , NULL);
-  // xTaskCreate( tiny_ml_task, "Tiny ML Task" ,2048  ,NULL  ,2 , NULL);
+  xTaskCreate(tiny_ml_task, "Tiny ML Task" ,2048  ,NULL  ,2 , NULL);
   // xTaskCreate(coreiot_task, "CoreIOT Task" ,4096  ,NULL  ,2 , NULL);
   // xTaskCreate(Task_Toogle_BOOT, "Task_Toogle_BOOT", 4096, NULL, 2, NULL);
 }
